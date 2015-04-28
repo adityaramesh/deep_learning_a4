40c7e404015762efd04efc6fccc6cc9a4e0c6fa8
Aditya Ramesh <_@adityaramesh.com>
2015-04-27 20:03:32 -0400

    Removed old files.

--- lstm/get_model.sh
+++
@@ -1,8 +0,0 @@
-#
-# File Name: get_model.sh
-# Author:    Aditya Ramesh
-# Date:      04/27/2015
-# Contact:   _@adityaramesh.com
-#
-
-wget https://github.com/adityaramesh/a4/releases/download/model/best_train_model.t7
